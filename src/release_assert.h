f267586defa5fed672d7e95ebedff9a24c21f762
Vincent Gerlach <58261670+RivinHD@users.noreply.github.com>
2025-12-11 10:22:06 +0100

    Fixed assert for mac

--- src/release_assert.h
+++ src/release_assert.h
@@ -2,6 +2,12 @@
 
 #include <sys/cdefs.h>
 
+#if __APPLE__
+#include <_assert.h>
+#define release_assert(expr)                                                                       \
+    (__builtin_expect(!(expr), 0) ? __assert_rtn(__func__, __ASSERT_FILE_NAME, __LINE__, #expr)    \
+                                  : (void)0)
+#else
 __BEGIN_DECLS
 
 /* Declaration extracted from assert.h*/
@@ -13,3 +19,4 @@ __END_DECLS
 
 #define release_assert(expr)                                                                       \
     ((expr) ? (void)0 : __assert_fail(#expr, __FILE__, __LINE__, __extension__ __PRETTY_FUNCTION__))
+#endif
