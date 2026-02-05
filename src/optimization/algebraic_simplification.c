b28f4b6b1ba31f7e456d2ce508ca1cc1d358670a
AsZen <92991272+Shad00Z@users.noreply.github.com>
2026-02-05 13:29:07 +0100

    feat: enhanced algebraic simplification

--- src/optimization/algebraic_simplification.c
+++ src/optimization/algebraic_simplification.c
@@ -1,4 +1,5 @@
 #include "ccngen/ast.h"
+#include "release_assert.h"
 #include <ccn/dynamic_core.h>
 #include <ccngen/enum.h>
 #include <stdbool.h>
@@ -11,6 +12,7 @@ node_st *OPT_ASbinop(node_st *node)
 
     if (BINOP_OP(node) == BO_mul)
     {
+        // NT_INT
         if (NODE_TYPE(BINOP_LEFT(node)) == NT_INT)
         {
             if (INT_VAL(BINOP_LEFT(node)) == 1)
@@ -43,6 +45,7 @@ node_st *OPT_ASbinop(node_st *node)
             }
         }
 
+        // NT_FLOAT
         if (NODE_TYPE(BINOP_LEFT(node)) == NT_FLOAT)
         {
             if (FLOAT_VAL(BINOP_LEFT(node)) == 1.0)
@@ -63,10 +66,10 @@ node_st *OPT_ASbinop(node_st *node)
         {
             if (FLOAT_VAL(BINOP_RIGHT(node)) == 1.0)
             {
-                node_st *right = BINOP_RIGHT(node);
-                BINOP_RIGHT(node) = NULL;
+                node_st *left = BINOP_LEFT(node);
+                BINOP_LEFT(node) = NULL;
                 CCNfree(node);
-                return right;
+                return left;
             }
             else if (FLOAT_VAL(BINOP_RIGHT(node)) == 0.0)
             {
@@ -75,12 +78,173 @@ node_st *OPT_ASbinop(node_st *node)
             }
         }
 
-        // TODO NT_BOOL
-    }
+        // NT_BOOL
+        if (NODE_TYPE(BINOP_LEFT(node)) == NT_BOOL)
+        {
+            if (BOOL_VAL(BINOP_LEFT(node)) == true)
+            {
+                node_st *right = BINOP_RIGHT(node);
+                BINOP_RIGHT(node) = NULL;
+                CCNfree(node);
+                return right;
+            }
+            else
+            {
+                CCNfree(node);
+                return ASTbool(false);
+            }
+        }
 
-    // TODO BO_add & BO_sub; NOTE neutral element 0 in both the same logic
-    // TODO BO_div == 1 && BO_rem == 1; NOTE a/1 = a = a%1
-    // TODO BO_and & BO_or
+        if (NODE_TYPE(BINOP_RIGHT(node)) == NT_BOOL)
+        {
+            if (BOOL_VAL(BINOP_RIGHT(node)) == true)
+            {
+                node_st *left = BINOP_LEFT(node);
+                BINOP_LEFT(node) = NULL;
+                CCNfree(node);
+                return left;
+            }
+            else
+            {
+                CCNfree(node);
+                return ASTbool(false);
+            }
+        }
+    }
+    else if (BINOP_OP(node) == BO_add || BINOP_OP(node) == BO_sub)
+    {
+        // TODO BO_add & BO_sub; NOTE neutral element 0 in both the same logic
+        if (NODE_TYPE(BINOP_LEFT(node)) == NT_INT)
+        {
+            if (INT_VAL(BINOP_LEFT(node)) == 0)
+            {
+                node_st *right = BINOP_RIGHT(node);
+                BINOP_RIGHT(node) = NULL;
+                CCNfree(node);
+                return right;
+            }
+        }
+
+        if (NODE_TYPE(BINOP_RIGHT(node)) == NT_INT)
+        {
+            if (INT_VAL(BINOP_RIGHT(node)) == 0)
+            {
+                node_st *left = BINOP_LEFT(node);
+                BINOP_LEFT(node) = NULL;
+                CCNfree(node);
+                return left;
+            }
+        }
+
+        // NT_FLOAT
+        if (NODE_TYPE(BINOP_LEFT(node)) == NT_FLOAT)
+        {
+            if (FLOAT_VAL(BINOP_LEFT(node)) == 0.0)
+            {
+                node_st *right = BINOP_RIGHT(node);
+                BINOP_RIGHT(node) = NULL;
+                CCNfree(node);
+                return right;
+            }
+        }
+
+        if (NODE_TYPE(BINOP_RIGHT(node)) == NT_FLOAT)
+        {
+            if (FLOAT_VAL(BINOP_RIGHT(node)) == 0.0)
+            {
+                node_st *left = BINOP_LEFT(node);
+                BINOP_LEFT(node) = NULL;
+                CCNfree(node);
+                return left;
+            }
+        }
+
+        // NT_BOOL
+        if (BINOP_OP(node) == BO_add)
+        {
+            if (NODE_TYPE(BINOP_LEFT(node)) == NT_BOOL)
+            {
+                if (BOOL_VAL(BINOP_LEFT(node)) == true)
+                {
+                    node_st *right = BINOP_RIGHT(node);
+                    BINOP_RIGHT(node) = NULL;
+                    CCNfree(node);
+                    return right;
+                }
+            }
+
+            if (NODE_TYPE(BINOP_RIGHT(node)) == NT_BOOL)
+            {
+                if (BOOL_VAL(BINOP_RIGHT(node)) == true)
+                {
+                    node_st *left = BINOP_LEFT(node);
+                    BINOP_LEFT(node) = NULL;
+                    CCNfree(node);
+                    return left;
+                }
+            }
+        }
+    }
+    else if (BINOP_OP(node) == BO_div || BINOP_OP(node) == BO_mod)
+    {
+        // TODO BO_div == 1 && BO_rem == 1; NOTE a/1 = a = a%1 (auch -1)
+        // DIV
+        // NT_INT
+        if (NODE_TYPE(BINOP_RIGHT(node)) == NT_INT)
+        {
+            if (INT_VAL(BINOP_RIGHT(node)) == 1)
+            {
+                node_st *left = BINOP_LEFT(node);
+                BINOP_LEFT(node) = NULL;
+                CCNfree(node);
+                return left;
+            }
+        }
+
+        if (NODE_TYPE(BINOP_RIGHT(node)) == NT_INT)
+        {
+            if (INT_VAL(BINOP_RIGHT(node)) == -1)
+            {
+                node_st *left = BINOP_LEFT(node);
+                node_st *new_left = ASTmonop(left, MO_neg);
+                BINOP_LEFT(node) = NULL;
+                CCNfree(node);
+                return new_left;
+            }
+        }
+
+        // NT_FLOAT
+        if (BINOP_OP(node) == BO_div)
+        {
+
+            if (NODE_TYPE(BINOP_RIGHT(node)) == NT_FLOAT)
+            {
+                if (FLOAT_VAL(BINOP_RIGHT(node)) == 1.0)
+                {
+                    node_st *left = BINOP_LEFT(node);
+                    BINOP_LEFT(node) = NULL;
+                    CCNfree(node);
+                    return left;
+                }
+            }
+
+            if (NODE_TYPE(BINOP_RIGHT(node)) == NT_FLOAT)
+            {
+                if (FLOAT_VAL(BINOP_RIGHT(node)) == -1.0)
+                {
+                    node_st *left = BINOP_LEFT(node);
+                    node_st *new_left = ASTmonop(left, MO_neg);
+                    BINOP_LEFT(node) = NULL;
+                    CCNfree(node);
+                    return new_left;
+                }
+            }
+        }
+    }
+    else if (BINOP_OP(node) == BO_and || BINOP_OP(node) == BO_or)
+    {
+        release_assert(false);
+    }
 
     return node;
 }
