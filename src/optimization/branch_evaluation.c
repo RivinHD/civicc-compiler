1893c3d7cbe7a9868d41ed0b798d9523040379d1
Vincent Gerlach <58261670+RivinHD@users.noreply.github.com>
2026-02-15 00:28:48 +0100

    feat(optimization): deeper lookup branch evaluation

--- src/optimization/branch_evaluation.c
+++ src/optimization/branch_evaluation.c
@@ -1,4 +1,8 @@
+#include "definitions.h"
+#include "palm/hash_table.h"
 #include "release_assert.h"
+#include "user_types.h"
+#include "utils.h"
 #include <ccn/dynamic_core.h>
 #include <ccn/phase_driver.h>
 #include <ccngen/ast.h>
@@ -6,17 +10,35 @@
 
 static node_st *extracted_stmts_first = NULL;
 static node_st *extracted_stmts_last = NULL;
+static htable_stptr assign_table = NULL;
+static bool conditioned_assign = false;
 
 void reset()
 {
     extracted_stmts_first = NULL;
     extracted_stmts_last = NULL;
+    assign_table = NULL;
+    conditioned_assign = false;
 }
 
 node_st *OPT_BEprogram(node_st *node)
 {
     reset();
+    assign_table = HTnew_String(2 << 4);
     TRAVchildren(node);
+    HTdelete(assign_table);
+    return node;
+}
+
+node_st *OPT_BEfundef(node_st *node)
+{
+    htable_stptr parent_temp_table = assign_table;
+    assign_table = HTnew_String(2 << 4);
+    bool success = HTinsert(assign_table, htable_parent_name, parent_temp_table);
+    release_assert(success);
+    TRAVchildren(node);
+    HTdelete(assign_table);
+    assign_table = parent_temp_table;
     return node;
 }
 
@@ -40,13 +62,59 @@ node_st *OPT_BEstatements(node_st *node)
     return node;
 }
 
-node_st *OPT_BEifstatement(node_st *node)
+node_st *OPT_BEassign(node_st *node)
 {
     TRAVchildren(node);
-    if (NODE_TYPE(IFSTATEMENT_EXPR(node)) == NT_BOOL)
+
+    char *name = VAR_NAME(ASSIGN_VAR(node));
+
+    // We need to remove any assign it was condition because we do not know the actual assigned
+    // value
+    if (conditioned_assign)
+    {
+        int level = 0;
+        void *entry = deep_lookup_level(assign_table, name, &level);
+        if (entry != NULL)
+        {
+            release_assert(level <= 0);
+            htable_stptr table = assign_table;
+            // we have to go - level - 1 steps upwards to find the correct table
+            while (level++ < -1)
+            {
+                table = HTlookup(table, htable_parent_name);
+            }
+
+            void *del = HTremove(table, name);
+            release_assert(del != NULL);
+        }
+    }
+    else
+    {
+        bool success = HTinsert(assign_table, name, ASSIGN_EXPR(node));
+        release_assert(success);
+    }
+    return node;
+}
+
+node_st *OPT_BEifstatement(node_st *node)
+{
+    node_st *expr = IFSTATEMENT_EXPR(node);
+
+    // We can extract values from temp variables to get possible further optimization
+    while (NODE_TYPE(expr) == NT_VAR)
+    {
+        node_st *entry = HTlookup(assign_table, VAR_NAME(expr));
+        if (entry == NULL)
+        {
+            break;
+        }
+        expr = entry;
+    }
+
+    if (NODE_TYPE(expr) == NT_BOOL)
     {
         // Remove the if and else block so it becomes dead code.
-        if (BOOL_VAL(IFSTATEMENT_EXPR(node)))
+        if (BOOL_VAL(expr))
         {
             extracted_stmts_first = IFSTATEMENT_BLOCK(node);
             node_st *stmts = IFSTATEMENT_BLOCK(node);
@@ -74,16 +142,31 @@ node_st *OPT_BEifstatement(node_st *node)
         IFSTATEMENT_BLOCK(node) = NULL;
         IFSTATEMENT_ELSE_BLOCK(node) = NULL;
     }
+
+    conditioned_assign = true;
+    TRAVchildren(node);
+    conditioned_assign = false;
+
     return node;
 }
 
 node_st *OPT_BEternary(node_st *node)
 {
-    TRAVchildren(node);
-    if (NODE_TYPE(TERNARY_PRED(node)) == NT_BOOL)
+    node_st *pred = TERNARY_PRED(node);
+    while (NODE_TYPE(pred) == NT_VAR)
+    {
+        node_st *entry = deep_lookup(assign_table, VAR_NAME(pred));
+        if (entry == NULL)
+        {
+            break;
+        }
+        pred = entry;
+    }
+
+    if (NODE_TYPE(pred) == NT_BOOL)
     {
         node_st *expr = NULL;
-        if (BOOL_VAL(TERNARY_PRED(node)))
+        if (BOOL_VAL(pred))
         {
             expr = TERNARY_PTRUE(node);
             TERNARY_PTRUE(node) = NULL;
@@ -95,34 +178,81 @@ node_st *OPT_BEternary(node_st *node)
         }
         CCNfree(node);
         CCNcycleNotify();
+        TRAVchildren(expr);
         return expr;
     }
+
+    TRAVchildren(node);
     return node;
 }
 
 node_st *OPT_BEwhileloop(node_st *node)
 {
-    TRAVchildren(node);
-    if (NODE_TYPE(WHILELOOP_EXPR(node)) == NT_BOOL && !BOOL_VAL(WHILELOOP_EXPR(node)))
+    node_st *expr = WHILELOOP_EXPR(node);
+    while (NODE_TYPE(expr) == NT_VAR)
+    {
+        node_st *entry = deep_lookup(assign_table, VAR_NAME(expr));
+        if (entry == NULL)
+        {
+            break;
+        }
+        expr = entry;
+    }
+
+    if (NODE_TYPE(expr) == NT_BOOL && !BOOL_VAL(expr))
     {
         // Remove the block so it becomes dead code
         CCNfree(WHILELOOP_BLOCK(node));
         WHILELOOP_BLOCK(node) = NULL;
         CCNcycleNotify();
     }
+
+    conditioned_assign = true;
+    TRAVchildren(node);
+    conditioned_assign = false;
+
     return node;
 }
 
 node_st *OPT_BEforloop(node_st *node)
 {
-    TRAVchildren(node);
-    if (NODE_TYPE(FORLOOP_COND(node)) == NT_INT &&
-        NODE_TYPE(ASSIGN_EXPR(FORLOOP_ASSIGN(node))) == NT_INT &&
-        (FORLOOP_ITER(node) == NULL || NODE_TYPE(FORLOOP_ITER(node)) == NT_INT))
+    node_st *expr = ASSIGN_EXPR(FORLOOP_ASSIGN(node));
+    node_st *cond = FORLOOP_COND(node);
+    node_st *iter = FORLOOP_ITER(node);
+    while (NODE_TYPE(expr) == NT_VAR)
     {
-        long start = INT_VAL(ASSIGN_EXPR(FORLOOP_ASSIGN(node)));
-        long end = INT_VAL(FORLOOP_COND(node));
-        long step = FORLOOP_ITER(node) == NULL ? 1 : INT_VAL(FORLOOP_ITER(node));
+        node_st *entry = deep_lookup(assign_table, VAR_NAME(expr));
+        if (entry == NULL)
+        {
+            break;
+        }
+        expr = entry;
+    }
+    while (NODE_TYPE(cond) == NT_VAR)
+    {
+        node_st *entry = deep_lookup(assign_table, VAR_NAME(cond));
+        if (entry == NULL)
+        {
+            break;
+        }
+        cond = entry;
+    }
+    while (iter != NULL && NODE_TYPE(iter) == NT_VAR)
+    {
+        node_st *entry = deep_lookup(assign_table, VAR_NAME(iter));
+        if (entry == NULL)
+        {
+            break;
+        }
+        iter = entry;
+    }
+
+    if (NODE_TYPE(cond) == NT_INT && NODE_TYPE(expr) == NT_INT &&
+        (FORLOOP_ITER(node) == NULL || NODE_TYPE(iter) == NT_INT))
+    {
+        long start = INT_VAL(expr);
+        long end = INT_VAL(cond);
+        long step = iter == NULL ? 1 : INT_VAL(iter);
 
         if ((step > 0 && (start + step >= end)) || (step < 0 && (start + step <= end)))
         {
@@ -132,13 +262,28 @@ node_st *OPT_BEforloop(node_st *node)
             CCNcycleNotify();
         }
     }
+
+    conditioned_assign = true;
+    TRAVchildren(node);
+    conditioned_assign = false;
+
     return node;
 }
 
 node_st *OPT_BEdowhileloop(node_st *node)
 {
-    TRAVchildren(node);
-    if (NODE_TYPE(DOWHILELOOP_EXPR(node)) == NT_BOOL && !BOOL_VAL(DOWHILELOOP_EXPR(node)))
+    node_st *expr = DOWHILELOOP_EXPR(node);
+    while (NODE_TYPE(expr) == NT_VAR)
+    {
+        node_st *entry = deep_lookup(assign_table, VAR_NAME(expr));
+        if (entry == NULL)
+        {
+            break;
+        }
+        expr = entry;
+    }
+
+    if (NODE_TYPE(expr) == NT_BOOL && !BOOL_VAL(expr))
     {
         // Extract the block, so it becomes dead code.
         extracted_stmts_first = DOWHILELOOP_BLOCK(node);
@@ -151,5 +296,7 @@ node_st *OPT_BEdowhileloop(node_st *node)
         DOWHILELOOP_BLOCK(node) = NULL;
         CCNcycleNotify();
     }
+
+    TRAVchildren(node);
     return node;
 }
